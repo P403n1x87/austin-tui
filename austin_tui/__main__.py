6a2cfc736ca8aaa6223f96971e5c6957ba625766
Ram Rachum <ram@rachum.com>
2020-10-28 16:39:35 +0200

    Update code to adhere to style standards

--- austin_tui/__main__.py
+++ austin_tui/__main__.py
@@ -33,11 +33,13 @@ from austin_tui.view import ViewBuilder
 from austin_tui.view.austin import AustinProfileMode, AustinView
 from psutil import Process
 
-try: 
-    get_all_tasks = asyncio.all_tasks # Python 3.7+
-except AttributeError:
-    get_all_tasks = asyncio.Task.all_tasks # Python 3.6 compatibility
 
+try: 
+    _get_all_tasks = asyncio.all_tasks  # Python 3.7+
+except AttributeError:
+    _get_all_tasks = asyncio.Task.all_tasks  # Python 3.6 compatibility
+
+    
 class AustinTUIArgumentParser(AustinArgumentParser):
     """Austin TUI implementation of the Austin argument parser."""
 
@@ -138,10 +140,10 @@ class AustinTUI(AsyncAustin):
         except AustinError:
             pass
 
-        for task in get_all_tasks():
+        for task in _get_all_tasks():
             task.cancel()
 
-        pending = [task for task in get_all_tasks() if not task.done()]
+        pending = [task for task in _get_all_tasks() if not task.done()]
         if pending:
             done, _ = asyncio.get_event_loop().run_until_complete(asyncio.wait(pending))
             for t in done:
